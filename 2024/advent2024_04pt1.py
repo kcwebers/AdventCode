ecd6427074009c30562588b0cbd4c444999c375e
kcwebers <kweberski@gmail.com>
2026-01-14 21:35:35 -0800

    day 4 initial commit

---
+++ 2024/advent2024_04pt1.py
@@ -0,0 +1,65 @@
+# --- Day 4: Ceres Search ---
+# "Looks like the Chief's not here. Next!" One of The Historians pulls out a device and pushes the only button on it. After a brief flash, you recognize the interior of the Ceres monitoring station!
+
+# As the search for the Chief continues, a small Elf who lives on the station tugs on your shirt; she'd like to know if you could help her with her word search (your puzzle input). She only has to find one word: XMAS.
+
+# This word search allows words to be horizontal, vertical, diagonal, written backwards, or even overlapping other words. It's a little unusual, though, as you don't merely need to find one instance of XMAS - you need to find all of them. Here are a few ways XMAS might appear, where irrelevant characters have been replaced with .:
+
+
+# ..X...
+# .SAMX.
+# .A..A.
+# XMAS.S
+# .X....
+# The actual word search will be full of letters instead. For example:
+
+# MMMSXXMASM
+# MSAMXMSMSA
+# AMXSXMAAMM
+# MSAMASMSMX
+# XMASAMXAMM
+# XXAMMXXAMA
+# SMSMSASXSS
+# SAXAMASAAA
+# MAMMMXMMMM
+# MXMXAXMASX
+
+# In this word search, XMAS occurs a total of 18 times; here's the same word search again, but where letters not involved in any XMAS have been replaced with .:
+
+# ....XXMAS.
+# .SAMXMS...
+# ...S..A...
+# ..A.A.MS.X
+# XMASAMX.MM
+# X.....XA.A
+# S.S.S.S.SS
+# .A.A.A.A.A
+# ..M.M.M.MM
+# .X.X.XMASX
+# Take a look at the little Elf's word search. How many times does XMAS appear?
+
+# input is a block of text with consistent line lengths
+# can break information into a matrix? no need to double split, just split once to get uniform rows of strings, then iterate through
+# check up, down, diagonal both forward and back
+# look for an x and then search the directions based on location in matrix 
+# - x must be at least in 4th row for upwards, upwards diagonals
+# - x must be at least in 4th spot for backwards, backwards diagonal
+# - x must be at least 4 from end for diagonal forward
+
+def find_xmas(input):
+    matrix = input.split("\n")
+
+    return matrix
+
+sample = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"""
+
+print(find_xmas(sample))
